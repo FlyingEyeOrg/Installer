b7b9bded1900ec5d6ccc28fe7d83a6e31606e368
UnknowName <13604461032@163.com>
2025-12-01 14:19:19 +0800

    完善使用 win32

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,136 @@
-int main()
+// WindowsProject1.cpp : 定义应用程序的入口点。
+//
+
+#include <Windows.h>
+
+#define MAX_LOADSTRING 100
+
+// 全局变量:
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING] = L"你好";                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING] = L"MyWindowsClass";            // 主窗口类名
+
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
 {
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 在此处放置代码。
+
+    // 初始化全局字符串
+    MyRegisterClass(hInstance);
+
+    // 执行应用程序初始化:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    MSG msg;
+
+    // 主消息循环:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, nullptr, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+//
+//  函数: MyRegisterClass()
+//
+//  目标: 注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex = {};  // 初始化为零
+
+    wcex.cbSize = sizeof(WNDCLASSEXW);  // 修正：使用WNDCLASSEXW而不是WNDCLASSEX
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIconW(nullptr, (LPCWSTR)IDI_APPLICATION);  // 使用LoadIconW
+    wcex.hCursor        = LoadCursorW(nullptr, (LPCWSTR)IDC_ARROW);      // 使用LoadCursorW
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName   = nullptr;  // 添加这一行
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIconW(nullptr, (LPCWSTR)IDI_APPLICATION);   // 添加小图标
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目标: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目标: 处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 在此处添加使用 hdc 的任何绘图代码...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
     return 0;
 }
