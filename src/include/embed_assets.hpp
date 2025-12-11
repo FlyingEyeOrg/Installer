6ecd4260dcdfb0b86bbba9ac3f65e3aa81fdf1d8
UnknowName <13604461032@163.com>
2025-12-11 20:30:19 +0800

    优化嵌入宏定义

--- src/include/embed_assets.hpp
+++ src/include/embed_assets.hpp
@@ -1,24 +1,33 @@
 #pragma once
 
-#define EMBED_FILE(var_name, file_path)                 \
+#ifdef CMAKE_SOURCE_DIR
+#define EMBED_UNIX_PATH(var_name, unix_file_path)            \
+    extern "C" {                                             \
+    extern const char binary_##var_name##_start[];           \
+    extern const char binary_##var_name##_end[];             \
+    extern const unsigned int binary_##var_name##_size;      \
+    }                                                        \
+    asm(".section .rdata,\"dr\"\n"                           \
+        ".globl binary_" #var_name                           \
+        "_start\n"                                           \
+        "binary_" #var_name                                  \
+        "_start:\n"                                          \
+        "    .incbin \"" CMAKE_SOURCE_DIR "/" unix_file_path \
+        "\"\n"                                               \
+        ".globl binary_" #var_name                           \
+        "_end\n"                                             \
+        "binary_" #var_name                                  \
+        "_end:\n"                                            \
+        ".globl binary_" #var_name                           \
+        "_size\n"                                            \
+        "binary_" #var_name                                  \
+        "_size:\n"                                           \
+        "    .long binary_" #var_name "_end - binary_" #var_name "_start\n");
+#else
+#define EMBED_UNIX_PATH(var_name, unix_file_path)       \
     extern "C" {                                        \
     extern const char binary_##var_name##_start[];      \
     extern const char binary_##var_name##_end[];        \
     extern const unsigned int binary_##var_name##_size; \
-    }                                                   \
-    asm(".section .rdata,\"dr\"\n"                      \
-        ".globl binary_" #var_name                      \
-        "_start\n"                                      \
-        "binary_" #var_name                             \
-        "_start:\n"                                     \
-        "    .incbin \"" file_path                      \
-        "\"\n"                                          \
-        ".globl binary_" #var_name                      \
-        "_end\n"                                        \
-        "binary_" #var_name                             \
-        "_end:\n"                                       \
-        ".globl binary_" #var_name                      \
-        "_size\n"                                       \
-        "binary_" #var_name                             \
-        "_size:\n"                                      \
-        "    .long binary_" #var_name "_end - binary_" #var_name "_start\n");
+    }
+#endif
