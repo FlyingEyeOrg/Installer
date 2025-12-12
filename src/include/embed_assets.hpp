6e4b36fc5c2c88e2f375127bea9ebc32805a9956
UnknowName <13604461032@163.com>
2025-12-12 11:15:17 +0800

    重命名 资源名称。

--- src/include/embed_assets.hpp
+++ src/include/embed_assets.hpp
@@ -1,33 +1,33 @@
 #pragma once
 
 #ifdef CMAKE_SOURCE_DIR_UNIX_PATH
-#define EMBED_UNIX_PATH(var_name, unix_file_path)                      \
+#define EMBED_UNIX_PATH(resource_name, unix_file_path)                      \
     extern "C" {                                                       \
-    extern const char binary_##var_name##_start[];                     \
-    extern const char binary_##var_name##_end[];                       \
-    extern const unsigned int binary_##var_name##_size;                \
+    extern const char binary_##resource_name##_start[];                     \
+    extern const char binary_##resource_name##_end[];                       \
+    extern const unsigned int binary_##resource_name##_size;                \
     }                                                                  \
     asm(".section .rdata,\"dr\"\n"                                     \
-        ".globl binary_" #var_name                                     \
+        ".globl binary_" #resource_name                                     \
         "_start\n"                                                     \
-        "binary_" #var_name                                            \
+        "binary_" #resource_name                                            \
         "_start:\n"                                                    \
         "    .incbin \"" CMAKE_SOURCE_DIR_UNIX_PATH "/" unix_file_path \
         "\"\n"                                                         \
-        ".globl binary_" #var_name                                     \
+        ".globl binary_" #resource_name                                     \
         "_end\n"                                                       \
-        "binary_" #var_name                                            \
+        "binary_" #resource_name                                            \
         "_end:\n"                                                      \
-        ".globl binary_" #var_name                                     \
+        ".globl binary_" #resource_name                                     \
         "_size\n"                                                      \
-        "binary_" #var_name                                            \
+        "binary_" #resource_name                                            \
         "_size:\n"                                                     \
-        "    .long binary_" #var_name "_end - binary_" #var_name "_start\n");
+        "    .long binary_" #resource_name "_end - binary_" #resource_name "_start\n");
 #else
-#define EMBED_UNIX_PATH(var_name, unix_file_path)       \
+#define EMBED_UNIX_PATH(resource_name, unix_file_path)       \
     extern "C" {                                        \
-    extern const char binary_##var_name##_start[];      \
-    extern const char binary_##var_name##_end[];        \
-    extern const unsigned int binary_##var_name##_size; \
+    extern const char binary_##resource_name##_start[];      \
+    extern const char binary_##resource_name##_end[];        \
+    extern const unsigned int binary_##resource_name##_size; \
     }
 #endif
